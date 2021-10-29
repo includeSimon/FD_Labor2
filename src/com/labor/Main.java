67174fc655c9bf42ba0ee9ef7ca28229c889036a
Pirlog Simon <simon.pirlog@yahoo.com>
2021-10-29 08:29:26 +0300

    Main geschrieben

--- src/com/labor/Main.java
+++ src/com/labor/Main.java
@@ -5,5 +5,12 @@ import test.BenutzerTest;
 public class Main {
 
     public static void main(String[] args) {
+        BenutzerTest test = new BenutzerTest();
+        test.setUp();
+        test.testGetters();
+        test.testSetters();
+        test.sportartZeitTest();
+        test.kalkuliereZeitTest();
+        test.kalkuliereDurchschnittsZeitTest();
     }
 }
